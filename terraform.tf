f70a8db178dc19ac1b6f2c0b9adce10cdf39653e
farrukh90 <farrukhsadykov@gmail.com>
2018-10-24 09:22:02 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,14 @@
-
 resource "aws_key_pair" "name" {
   	key_name = "name"
         public_key = ""       or    "${file("public_key")}"
 }
-resource "aws_instance" "my-instance" {
-	ami	=	"ami-0b59bfac6be064b78"
-	instance_type	=	"t2.micro"
-        key_name = "${aws_key_pair.name.name}"
-	tags {
-	NAME = ""
-	}
+esource "aws_instance" "my-instance" {
+  ami           = "${var.awsami}"
+  instance_type = "${var.micro}"
+  key_name      = "${aws_key_pair.somekey.key_name}"
+  user_data     = "${file("user_data")}"
+  count		= "${var.instance_count}"
+  tags {
+    Environment = "dev-${"count.index +1}"
+  }
 }
